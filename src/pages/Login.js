08448635a110bcea45844f84214b74fe9034f3d6
Victor Duque <victor.duque11@hotmail.com>
2023-05-12 20:02:27 -0300

    foi criada a rota, página login e rota pagina wallet, store e reducer

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,60 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  btnDisabled() {
+    const { email, password } = this.state;
+    const regex = /\w+@\w+\.\w{2,8}(\.\w{0,2})?/g;
+    const validateEmail = regex.test(email);
+    const passSize = 6;
+
+    if (validateEmail && password.length >= passSize) {
+      return false;
+    }
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <div className="login-container">
+        <h1>Trybe Wallet</h1>
+        <h3>Login</h3>
+        <div className="input-container">
+          <input
+            type="email"
+            name="email"
+            data-testid="email-input"
+            value={ email }
+            placeholder="e-mail"
+            onChange={ (element) => this.setState({ email: element.target.value }) }
+          />
+          <input
+            type="password"
+            name="password"
+            data-testid="password-input"
+            value={ password }
+            placeholder="password"
+            onChange={ (element) => this.setState({ password: element.target.value }) }
+          />
+        </div>
+        <div className="btn-container">
+          <button
+            type="button"
+            disabled={ this.btnDisabled(email, password) }
+            onClick={ () => console.log('btnEntrar') }
+          >
+            Entrar
+          </button>
+        </div>
+      </div>
+    );
   }
 }
 
