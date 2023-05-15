93fe07a548b6d3f57b8fbe60e9ac51abd69432fa
Victor Duque <victor.duque11@hotmail.com>
2023-05-15 20:56:40 -0300

    header criado e aparecendo, requisito 2 pronto

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Header extends Component {
   render() {
+    const { user } = this.props;
     return (
-      <div>Header</div>
+      <div className="header-container">
+        <span className="email-field" data-testid="email-field">
+          { user.email }
+        </span>
+        <div>
+          <span className="total-field" data-testid="total-field">
+            0
+          </span>
+          <span className="header-currency-field" data-testid="header-currency-field">
+            BRL
+          </span>
+        </div>
+      </div>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  user: state.user,
+});
+
+Header.propTypes = {
+  user: PropTypes.objectOf(String),
+}.isRequired;
+
+export default connect(mapStateToProps)(Header);
